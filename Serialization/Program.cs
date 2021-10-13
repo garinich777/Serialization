9584630c009f29914e778b7c4c2170065ce93f3d
Igor Osipov <62898482+garinich777@users.noreply.github.com>
2021-10-14 00:20:46 +0300

    modified class ListRandom

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -10,7 +10,23 @@ namespace Serialization
     {
         static void Main(string[] args)
         {
-
+            ListRandom listRandom = new ListRandom();
+            listRandom.Add("Тест");
+            listRandom.Add("Тест1");
+            listRandom.Add("Тест2");
+            foreach (var item in listRandom)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            listRandom.Remove("Тест1");
+            foreach (var item in listRandom)
+            {
+                Console.WriteLine(item);
+            }
+            int a = listRandom.Count;
+            listRandom.Clear();
+            int b = listRandom.Count;
         }
     }
 }
--- Serialization/СustomList.cs
+++ Serialization/СustomList.cs
@@ -1,28 +1,99 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Serialization
 {
-    class ListNode
+    public class ListNode
     {
+        public ListNode(string data)
+        {
+            Data = data;
+        }
+
         public ListNode Previous;
         public ListNode Next;
         public ListNode Random; // произвольный элемент внутри списка
         public string Data;
     }
-    class ListRandom
+
+    public class ListRandom : IEnumerable
     {
-        public ListNode Head;
-        public ListNode Tail;
-        public int Count;
+        ListNode Head;
+        ListNode Tail;
+        int _count;
+
+        public int Count { get { return _count; } }
+        public bool IsEmpty { get { return _count == 0; } }
+
+
         public void Serialize(Stream s)
         {
         }
         public void Deserialize(Stream s)
         {
+        }
+
+        public void Add(string data)
+        {
+            ListNode node = new ListNode(data);
+            if (Head == null)
+                Head = node;
+            else
+                Tail.Next = node;
+            Tail = node;
+            _count++;
+        }
+
+        public bool Remove(string data)
+        {
+            ListNode current = Head;
+            ListNode previous = null;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next == null)
+                            Tail = previous;
+                    }
+                    else
+                    {
+                        Head = Head.Next;
+                        if (Head == null)
+                            Tail = null;
+                    }
+                    _count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            _count = 0;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            ListNode current = Head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
         }
     }
 }
